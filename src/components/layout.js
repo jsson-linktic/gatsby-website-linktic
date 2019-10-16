2a67bd7a0170fb6f9f74f766f60ce4aee0bfd659
Jeisson Rodriguez <jeison.camilo02@gmail.com>
2019-10-16 00:58:28 -0500

    front-end

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,41 +1,47 @@
 import React from "react"
 import { Link } from "gatsby"
-import './css/webflow.css'
+
+// css
 import './css/normalize.css'
+import './css/webflow.css'
 import './css/etanwebflow.css'
+import './css/styles.css'
+
 //import './js/webflow.js'
 const ListLink = props => (
-  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+  <a  class="nav-link on-dark w-nav-link">
     <Link to={props.to}>{props.children}</Link>
-  </li>
+  </a>
 )
 
 export default ({ children }) => (
   <>
-    <div class="section-header position-absolute">
-      <div class="container space-between align-middle">
-        <div data-collapse="medium" data-animation="default" data-duration="400" class="navbar w-nav">
-          <div class="navbar-container align-middle w-container">
-            <div class="col lg-4 order-first no-padding-left no-margin-bottom flex-horizontal lg-2 md-4 xs-8">
-              <div class="menu-and-logo-container order-first"><a data-w-id="7d050a59-b525-e710-dbfb-70b18fafaf10" href="#" class="offcanvas-button margin-right w-inline-block"><img src={require('../../images/burger-menu-icon-white.svg')} alt=""/></a>
-                <span class="brand w-nav-brand">
-                  <h3 class="logotype on-dark">e—<br/><ListLink to="/">tan</ListLink></h3>
-                </span>
-              </div>
+    <div className="section-header position-absolute">
+      <div className="container space-between align-middle">
+        <div data-collapse="medium" data-animation="default" data-duration="400" className="navbar w-nav">
+          <div className="navbar-container align-middle w-container">
+            <div className="col lg-4 order-first no-padding-left no-margin-bottom flex-horizontal lg-2 md-4 xs-8">
+              <ListLink to="/">
+                <div class="menu-and-logo-container order-first"><a data-w-id="7d050a59-b525-e710-dbfb-70b18fafaf10" class="offcanvas-button margin-right w-inline-block"><img src={require('../../images/burger-menu-icon-white.svg')} alt=""/></a>
+                  <a class="brand w-nav-brand">
+                    <h3 class="logotype on-dark">e—<br/>tan</h3>
+                  </a>
+                </div>
+              </ListLink>
             </div>
-            <div class="col lg-4 no-margin-bottom flexh-justify-center position-absolute-md lg-8">
-              <nav role="navigation" class="nav-menu w-nav-menu"><span clspanss="nav-link on-dark w-nav-link"><ListLink to="/about/">About</ListLink></span>
-                <div data-hover="1" data-delay="0" data-w-id="7d050a59-b525-e710-dbfb-70b18fafaf1a" class="nav-dropdown w-dropdown">
-                  <div class="nav-link on-dark w-dropdown-toggle">
-                    <div class="w-icon-dropdown-toggle"></div>
-                    <div><ListLink to="/about/">Our work</ListLink></div>
+            <div className="col lg-4 no-margin-bottom flexh-justify-center position-absolute-md lg-8">
+              <nav role="navigation" className="nav-menu w-nav-menu"><ListLink to="/about/"><a clspanss="nav-link on-dark w-nav-link">About</a></ListLink>
+                <div data-hover="1" data-delay="0" data-w-id="7d050a59-b525-e710-dbfb-70b18fafaf1a" className="nav-dropdown w-dropdown">
+                  <div className="nav-link on-dark w-dropdown-toggle">
+                    <div className="w-icon-dropdown-toggle"></div>
+                    <ListLink to="/about/"><div>Our work</div></ListLink>
                   </div>
-                  <nav class="dropdown-list w-dropdown-list"><a href="our-work.html" class="dropdown-link w-dropdown-link">Fullwidth gallery</a><a href="our-work-2.html" class="dropdown-link w-dropdown-link">Standard gallery</a></nav>
-                </div><span class="nav-link on-dark w-nav-link"><ListLink to="/services/">Services</ListLink></span><span class="nav-link on-dark w-nav-link"><ListLink to="/news/">News</ListLink></span><span class="nav-link on-dark w-nav-link"><ListLink to="/contact-us/">Contact</ListLink> </span></nav>
+                  <nav className="dropdown-list w-dropdown-list"><ListLink to="/our-work/"><a className="dropdown-link w-dropdown-link">Fullwidth gallery</a></ListLink><ListLink to="/our-work-2/"><a className="dropdown-link w-dropdown-link">Standard gallery</a></ListLink></nav>
+                </div><span className="nav-link on-dark w-nav-link"><ListLink to="/services/">Services</ListLink></span><span className="nav-link on-dark w-nav-link"><ListLink to="/news/">News</ListLink></span><span className="nav-link on-dark w-nav-link"><ListLink to="/contact-us/">Contact</ListLink> </span></nav>
             </div>
-            <div class="menu-button w-nav-button"><img src={require('../../images/burger-menu-icon-white.svg')} alt="" class="burger-menu-icon"/></div>
-            <div class="col lg-4 no-margin-bottom flexh-justify-end no-padding-right grow hidden-xs lg-2">
-              <div class="extra-menu"><span class="extra-menu-link margin-left nav-link on-dark"><ListLink to="/hiring/">We&#x27;re hiring</ListLink></span></div>
+            <div className="menu-button w-nav-button"><img src={require('../../images/burger-menu-icon-white.svg')} alt="" className="burger-menu-icon"/></div>
+            <div className="col lg-4 no-margin-bottom flexh-justify-end no-padding-right grow hidden-xs lg-2">
+              <div className="extra-menu"><span className="extra-menu-link margin-left nav-link on-dark"><ListLink to="/hiring/">We&#x27;re hiring</ListLink></span></div>
             </div>
           </div>
         </div>
---
+++ src/pages/our-work-2.js
@@ -0,0 +1,190 @@
+import React, { Component } from 'react';
+import Layout from "../components/layout";
+
+class ourwork2 extends Component {
+    render() {
+        return (
+            <Layout title="our-work">
+                
+                <div class="section is-header bg-accent-dark position-relative has-bg-img img2">
+                    <div class="gradient-overlay"></div>
+                    <div class="container flex-horizontal position-relative">
+                    <div class="col lg-8 md-12">
+                        <div class="breadcrumb margin-bottom-double"><a href="#">Home</a><span class="fa margin-left-small margin-right-small is-xsmall"></span>Our work</div>
+                        <div class="header-content-wrapper-2">
+                        <div class="pre-title-container">
+                            <div class="pre-title is-left-aligned">2847 projects competed and counting</div>
+                            <div class="pre-title-decoration"></div>
+                        </div>
+                        <h1>Case studies</h1>
+                        <div class="text-medium">We value our craft, have a deep passion for progression, and exist to solve challenges for brands we believe in</div>
+                        </div>
+                    </div>
+                    <div class="col lg-1"></div>
+                    <div class="col lg-6 no-margin-bottom md-12"></div>
+                    </div>
+                </div>
+                <div class="section position-relative overflow-hidden">
+                    <div class="container">
+                    <div class="col lg-7">
+                        <h2>We collaborate with ambitious brands and people; we’d love to build something great together.</h2>
+                    </div>
+                    </div>
+                    <div class="container">
+                    <div class="col lg-12 no-padding-lr">
+                        <div class="w-layout-grid c-grid3x3-2">
+                        <a data-w-id="0827c0ac-f85e-ac13-d953-657f09a45be6" href="#" class="c-grid1__item-3 w-inline-block">
+                            <div class="c-grid1__content-2 zindex5">
+                            <div class="c-grid1__partner">Google</div>
+                            <p class="paragraph">Google Partnership — Bringing the best of Google to people across the globe<br/></p>
+                            </div>
+                            <div class="c-grid1__image our-work-2"></div>
+                        </a>
+                        <a data-w-id="0827c0ac-f85e-ac13-d953-657f09a45bee" href="#" class="c-grid1__item-3 w-inline-block">
+                            <div class="c-grid1__content-2 zindex5">
+                            <div class="c-grid1__partner">beats by dre</div>
+                            <p class="paragraph">BeatsByDre.com — Leading a digital transformation &amp; eComm redesign<br/></p>
+                            </div>
+                            <div class="c-grid1__image img2"></div>
+                        </a>
+                        <a data-w-id="0827c0ac-f85e-ac13-d953-657f09a45bf6" href="#" class="c-grid1__item-3 w-inline-block">
+                            <div class="c-grid1__content-2 zindex5">
+                            <div class="c-grid1__partner">crafted</div>
+                            <p class="paragraph">Crafted — Giving creators of all disciplines a seat at the table<br/></p>
+                            </div>
+                            <div class="c-grid1__image img3"></div>
+                        </a>
+                        <a data-w-id="0827c0ac-f85e-ac13-d953-657f09a45bfe" href="#" class="c-grid1__item-3 w-inline-block">
+                            <div class="c-grid1__content-2 zindex5">
+                            <div class="c-grid1__partner">BB Dakota</div>
+                            <p class="paragraph">BBDakota.com — Bringing storytelling and personality to eCommerce<br/></p>
+                            </div>
+                            <div class="c-grid1__image img4"></div>
+                        </a>
+                        <a data-w-id="0827c0ac-f85e-ac13-d953-657f09a45c06" href="#" class="c-grid1__item-3 w-inline-block">
+                            <div class="c-grid1__content-2 zindex5">
+                            <div class="c-grid1__partner">djavaweb</div>
+                            <p class="paragraph">Culture Manual — Realigning company culture through brand &amp; digital<br/>Explore Case Study<br/></p>
+                            </div>
+                            <div class="c-grid1__image img5"></div>
+                        </a>
+                        <a data-w-id="0827c0ac-f85e-ac13-d953-657f09a45c10" href="#" class="c-grid1__item-3 w-inline-block">
+                            <div class="c-grid1__content-2 zindex5">
+                            <div class="c-grid1__partner">softbank vision fund</div>
+                            <p class="paragraph">VisionFund.com — Digitally enriching a multibillion-dollar enterprise<br/></p>
+                            </div>
+                            <div class="c-grid1__image img6"></div>
+                        </a>
+                        <a data-w-id="0827c0ac-f85e-ac13-d953-657f09a45c18" href="#" class="c-grid1__item-3 w-inline-block">
+                            <div class="c-grid1__content-2 zindex5">
+                            <div class="c-grid1__partner">heath ceramic</div>
+                            <p class="paragraph">HeathCeramics.com - An eCommerce experience as crafted as their products<br/></p>
+                            </div>
+                            <div class="c-grid1__image img7"></div>
+                        </a>
+                        <a data-w-id="0827c0ac-f85e-ac13-d953-657f09a45c20" href="#" class="c-grid1__item-3 w-inline-block">
+                            <div class="c-grid1__content-2 zindex5">
+                            <div class="c-grid1__partner">OXO</div>
+                            <p class="paragraph">OXO.com — Bridging the gap between kitchen and keyboard<br/></p>
+                            </div>
+                            <div class="c-grid1__image img8"></div>
+                        </a>
+                        <a data-w-id="0827c0ac-f85e-ac13-d953-657f09a45c28" href="#" class="c-grid1__item-3 w-inline-block">
+                            <div class="c-grid1__content-2 zindex5">
+                            <div class="c-grid1__partner">Nixon</div>
+                            <p class="paragraph">Nixon.com — An eComm &amp; digital transformation driving global growth<br/></p>
+                            </div>
+                            <div class="c-grid1__image img9"></div>
+                        </a>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="background-img img1 align-right push-from-lg-7"></div>
+                </div>
+                <div class="section position-relative">
+                    <div class="container flex-horizontal">
+                    <div class="col lg-3"></div>
+                    <div class="col lg-7 md-12">
+                        <div class="pre-title-container">
+                        <div class="pre-title is-left-aligned">let&#x27;s work together</div>
+                        <div class="pre-title-decoration"></div>
+                        </div>
+                        <h2>We can help your business grow</h2>
+                        <div>Let&#x27;s have a friendly chat to find out</div>
+                    </div>
+                    <div class="col lg-2 md-12 flexv-justify-center"><a href="contact-us.html" class="w-inline-block"><img src="https://via.placeholder.com/1000x600.png?text=IMAGE" alt=""/></a></div>
+                    </div>
+                    <div class="background-img img1 align-left push-to-lg-3 style2"></div>
+                </div>
+                <div class="section section-footer-dark padding-bottom-16">
+                    <div class="container margin-bottom">
+                    <div class="col lg-2 md-12 md-order-first">
+                        <h3 class="logotype on-dark">e—<br/>tan</h3>
+                    </div>
+                    <div class="col lg-6 md-12 no-margin-bottom">
+                        <div class="container container-nested">
+                        <div class="col lg-4 md-12">
+                            <h4>Service</h4><a href="#" class="footer-nav-link on-dark">Development team</a><a href="#" class="footer-nav-link on-dark">Mobile app</a><a href="#" class="footer-nav-link on-dark">Enterprise software</a><a href="#" class="footer-nav-link on-dark">DevOps</a></div>
+                        <div class="col lg-4 md-12">
+                            <h4>Company</h4><a href="#" class="footer-nav-link on-dark">About</a><a href="#" class="footer-nav-link on-dark">Our Work</a><a href="#" class="footer-nav-link on-dark">News</a><a href="#" class="footer-nav-link on-dark">Career</a></div>
+                        <div class="col lg-4 no-margin-bottom-lg md-12">
+                            <h4>Let&#x27;s connect</h4>
+                            <a href="#" class="footer-nav-link-2 on-dark w-inline-block">
+                            <div><span class="fa-brand w24"></span> LinkedIn</div>
+                            </a>
+                            <a href="#" class="footer-nav-link-2 on-dark w-inline-block">
+                            <div><span class="fa-brand w24"></span> Youtube</div>
+                            </a>
+                            <a href="#" class="footer-nav-link-2 on-dark w-inline-block">
+                            <div><span class="fa-brand w24"></span> Facebook</div>
+                            </a>
+                            <a href="#" class="footer-nav-link-2 on-dark w-inline-block">
+                            <div><span class="fa-brand w24"></span> Instagram</div>
+                            </a>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="col lg-4 md-12 no-margin-bottom">
+                        <h4 class="margin-bottom">Subscribe</h4>
+                        <div class="margin-bottom w-form">
+                        <form id="email-form" name="email-form" data-name="Email Form" class="flexh-align-center"><input type="email" class="form-input-text is-grey margin-right-small no-margin-bottom md-margin-bottom w-input" maxlength="256" name="email-2" data-name="Email 2" placeholder="Enter your email address" id="email-2" required=""/><input type="submit" value="Subscribe" data-wait="Please wait..." class="button-primary button-primary-small w-button"/></form>
+                        <div class="w-form-done">
+                            <div>Thank you! Your submission has been received!</div>
+                        </div>
+                        <div class="w-form-fail">
+                            <div>Oops! Something went wrong while submitting the form.</div>
+                        </div>
+                        </div>
+                        <div class="margin-bottom low-text-contrast text-small">Join our newsletter to stay up to date on features and releases</div>
+                    </div>
+                    </div>
+                    <div class="container">
+                    <div class="col lg-12 margin-bottom">
+                        <div class="hr"></div>
+                    </div>
+                    </div>
+                    <div class="container">
+                    <div class="col lg-6 no-margin-bottom md-12 md-order-last">
+                        <div class="low-text-contrast text-small flexh-space-between md-flex-vertical">
+                        <div class="md-order-last">©2019 Etan Kali, Co. All rights reserved. </div><a href="#" class="footer-bottom-link">Privacy Policy</a><a href="#" class="footer-bottom-link">Term Of Service</a></div>
+                    </div>
+                    <div class="col lg-6 no-margin-bottom md-12">
+                        <div class="w100 text-align-right footer-bottom md-text-align-left margin-bottom">Made in Surabaja, Indonesia</div>
+                    </div>
+                    </div>
+                </div>
+                <div class="offcanvas-overlay">
+                    <div class="offcanvas-menu-list">
+                    <div data-w-id="5362a795-2c72-f81a-26bc-3be66ae3f924" class="fa on-dark close-button-offcanvas"></div>
+                    <div class="offcanvas-menu-item-wrapper"><a href="#" class="offcanvas-menu-item item1">About Us</a></div>
+                    <div class="offcanvas-menu-item-wrapper"><a href="#" class="offcanvas-menu-item item2">Features</a></div>
+                    <div class="offcanvas-menu-item-wrapper"><a href="#" class="offcanvas-menu-item item3">Our Team</a></div>
+                    <div class="offcanvas-menu-item-wrapper"><a href="#" class="offcanvas-menu-item item4">Pricing</a></div>
+                    </div>
+                </div>
+            </Layout>
+        );
+    }
+}
+
+export default ourwork2;
